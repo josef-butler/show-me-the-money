e025cbb20541f1a405050443a934296f2af31dce
fai-songprasit <prapawan.songprasit@gmail.com>
2020-07-09 18:54:39 +1200

    change order of render for navbar

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -31,9 +31,9 @@ class Nav extends React.Component {
             { auth.isAuthenticated
               ? (
                 <>
-                  <Link to='/' className="navbar-item is-large" onClick={() => logout()}>Logout</Link>
                   <Link to='/dashboard' className="navbar-item is-large">Dashboard</Link>
                   <Link to='/create' className="navbar-item is-large">Create Meeting</Link>
+                  <Link to='/' className="navbar-item is-large" onClick={() => logout()}>Logout</Link>
                 </>
                 )
               : (
