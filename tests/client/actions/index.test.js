62a435a53599241a4c6096c1a13289145a981926
andrewtempany <andrewtempany95@gmail.com>
2020-07-09 19:37:57 +1200

    added a single action test

--- tests/client/actions/index.test.js
+++ tests/client/actions/index.test.js
@@ -1,4 +1,7 @@
 import {requestLogin} from '../../../client/actions/auth'
+import jest from 'jest'
+import {addStaticData} from '../../../client/actions/staticActions'
+import {ADD_STATE} from '../../../client/actions/staticActions'
 
 test('Login request', () => {
   // Arrange
@@ -13,4 +16,17 @@ test('Login request', () => {
   expect(actual).toHaveProperty('isAuthenticated')
 
   expect(actual.type).toEqual(expectedType)
+})
+
+test('Jest is working', () => {
+  expect(8).toEqual(8)
+})
+
+test('Add StaticData action works', () => {
+  const expected = {
+    type: ADD_STATE,
+    data: 'I AM SENTIENT DATA'
+  }
+  let actual = addStaticData('I AM SENTIENT DATA')
+  expect(actual).toEqual(expected)
 })
