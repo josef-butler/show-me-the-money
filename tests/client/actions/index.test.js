67c5ef29524db897f141edce279eed3d6be2def2
andrewtempany <andrewtempany95@gmail.com>
2020-07-10 07:38:47 +1200

    add 2 action tests

--- tests/client/actions/index.test.js
+++ tests/client/actions/index.test.js
@@ -1,11 +1,15 @@
-import {requestLogin} from '../../../client/actions/auth'
+import { requestLogin } from '../../../client/actions/auth'
 import jest from 'jest'
-import {addStaticData} from '../../../client/actions/staticActions'
-import {ADD_STATE} from '../../../client/actions/staticActions'
+import { addStaticData } from '../../../client/actions/staticActions'
+import { ADD_STATE } from '../../../client/actions/staticActions'
+import { tickOneSecond } from '../../../client/actions/dynamicMeeting'
+import { TICK_ONE_SECOND } from '../../../client/actions/dynamicMeeting'
+import { RECEIVE_MEETINGS } from '../../../client/actions/meetings'
+import { receiveMeetings } from '../../../client/actions/meetings'
 
 test('Login request', () => {
   // Arrange
-  const expectedType  = 'LOGIN_REQUEST'
+  const expectedType = 'LOGIN_REQUEST'
 
   // Act
   const actual = requestLogin()
@@ -28,5 +32,23 @@ test('Add StaticData action works', () => {
     data: 'I AM SENTIENT DATA'
   }
   let actual = addStaticData('I AM SENTIENT DATA')
+  expect(actual).toEqual(expected)
+})
+
+test('Tick one second action works', () => {
+  const expected = {
+    type: TICK_ONE_SECOND,
+    cps: "FREEEEE MEEEEE",
+  }
+  let actual = tickOneSecond("FREEEEE MEEEEE")
+  expect(actual).toEqual(expected)
+})
+
+test('ReceiveMeetings action works', () => {
+  const expected = {
+    type: RECEIVE_MEETINGS,
+    meetings: 'I CANT FEEL MY TOES!',
+  }
+  let actual = receiveMeetings('I CANT FEEL MY TOES!')
   expect(actual).toEqual(expected)
 })
