7e48c09f72587c1465304e3fecbc3f131a1a3387
Joe Butler <joefish.b@gmail.com>
2020-07-09 12:31:11 +1200

    Jumble dates so they arent in order for testing

--- seeds/meetings.js
+++ seeds/meetings.js
@@ -8,7 +8,7 @@ exports.seed = function (knex) {
         {
           id: 1,
           meeting_name: "Bananas",
-          time: 1594254200000,
+          time: 1591575018000,
           duration: 58,
           attendees: 5,
           cost: 0.5,
@@ -16,7 +16,7 @@ exports.seed = function (knex) {
         {
           id: 2,
           meeting_name: "Salt",
-          time: 1591575018000,
+          time: 1583109018000,
           duration: 58,
           attendees: 7,
           cost: 0.6,
@@ -32,7 +32,7 @@ exports.seed = function (knex) {
         {
           id: 4,
           meeting_name: "Happy",
-          time: 1583109018000,
+          time: 1594254200000,
           duration: 65,
           attendees: 20,
           cost: 2.5,
@@ -40,7 +40,7 @@ exports.seed = function (knex) {
         {
           id: 5,
           meeting_name: "Dingo",
-          time: 1583152218000,
+          time: 1583107818000,
           duration: 34,
           attendees: 11,
           cost: 25.5,
