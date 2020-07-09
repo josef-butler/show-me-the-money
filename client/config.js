1de85cdc2685c54469d138d19e0a0595316b8b4b
benjamin-means <ben.w.means@gmail.com>
2020-07-09 17:44:22 +1200

    another shot in dark

--- client/config.js
+++ client/config.js
@@ -1,7 +1,7 @@
 const environments = {
   development: 'http://localhost:3000/api/v1',
   test: 'http://localhost:3333/api/v1',
-  production: process.env.NODE_ENV,
+  production: '',
   integration: '',
   deployment: '',
   build: ''
